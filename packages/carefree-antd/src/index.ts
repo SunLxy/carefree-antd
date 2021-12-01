e24954dea9618763dd0fe9348f7d07327eb9b29b
SunLxy <1011771396@qq.com>
2021-12-01 09:11:46 +0800

    chore:版本依赖

--- packages/carefree-antd/src/index.ts
+++ packages/carefree-antd/src/index.ts
@@ -1,2 +1,5 @@
-export { default as SimpleForm } from "carefree-antd-form"
-export * from "carefree-antd-form"
+export { default as SimpleForm } from 'carefree-antd-form';
+export * from 'carefree-antd-form';
+
+export { default as EditTable } from 'carefree-antd-edit-table';
+export * from 'carefree-antd-edit-table';
