99390d2e0d51be545b3e37b646ce51fdd1892262
SunLxy <1011771396@qq.com>
2021-12-10 19:55:51 +0800

    fix:修改ci脚本

--- packages/carefree-antd/src/index.ts
+++ packages/carefree-antd/src/index.ts
@@ -3,3 +3,6 @@ export * from 'carefree-antd-form';
 
 export { default as EditTable } from 'carefree-antd-edit-table';
 export * from 'carefree-antd-edit-table';
+
+export { default as ProTable } from 'carefree-pro-table';
+export * from 'carefree-pro-table';
