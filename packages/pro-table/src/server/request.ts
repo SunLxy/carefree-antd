a3802dba30e1988b5a86bdbac1aa8b4cb666cbd7
SunLxy <1011771396@qq.com>
2021-12-16 20:30:56 +0800

    fix:修改pro-table request方法

--- packages/pro-table/src/server/request.ts
+++ packages/pro-table/src/server/request.ts
@@ -1,52 +1,44 @@
 import request, { RequestOptionsInit, RequestResponse } from 'umi-request';
 import queryString from 'query-string';
-// 处理数据 转换
-request.interceptors.request.use(
-  (url, options: RequestOptionsInit & { module?: string }) => {
-    const { requestType, headers, data, module, ...rest } = options;
-    let head = { ...(headers || {}) };
-    let body = data;
-    if (requestType === 'json') {
-      head = {
-        Accept: 'application/json',
-        'Content-Type': 'application/json;charset=UTF-8',
-        ...head,
-      };
-      if (typeof body !== 'string') {
-        body = JSON.stringify(body);
-      }
-    } else if (requestType === 'form') {
-      head = {
-        Accept: 'application/json',
-        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
-        ...head,
-      };
-      body = typeof body === 'string' ? body : queryString.stringify(body);
-    } else {
-      head = {
-        Accept: 'application/json',
-        ...head,
-      };
-    }
-    let path = url;
-    if (process.env.NODE_ENV === 'production' && module) {
-      const pre = /^\//.test(url);
-      path = `/${module}${pre ? '' : '/'}${url}`;
-    }
-    return {
-      url: path,
-      options: {
-        requestType,
-        headers: { ...head },
-        data: body,
-        ...rest,
-      },
+const API_Request = (
+  url: string,
+  options: RequestOptionsInit & { module?: string },
+) => {
+  const { requestType, headers, data, module, ...rest } = options;
+  let head = { ...(headers || {}) };
+  let body = data;
+  if (requestType === 'json') {
+    head = {
+      Accept: 'application/json',
+      'Content-Type': 'application/json;charset=UTF-8',
+      ...head,
     };
-  },
-);
-
-request.interceptors.response.use((response: RequestResponse['response']) => {
-  return response;
-});
-
-export default request;
+    if (typeof body !== 'string') {
+      body = JSON.stringify(body);
+    }
+  } else if (requestType === 'form') {
+    head = {
+      Accept: 'application/json',
+      'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+      ...head,
+    };
+    body = typeof body === 'string' ? body : queryString.stringify(body);
+  } else {
+    head = {
+      Accept: 'application/json',
+      ...head,
+    };
+  }
+  let path = url;
+  if (process.env.NODE_ENV === 'production' && module) {
+    const pre = /^\//.test(url);
+    path = `/${module}${pre ? '' : '/'}${url}`;
+  }
+  return request(path, {
+    requestType,
+    headers: { ...head },
+    data: body,
+    ...rest,
+  });
+};
+export default API_Request;
