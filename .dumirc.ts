37c31d2b917d23873ddadd26372efad3bd6cf6c0
SunLxy <1011771396@qq.com>
2024-07-07 20:22:42 +0800

    fix:修改文档打包

--- .dumirc.ts
+++ .dumirc.ts
@@ -8,6 +8,8 @@ export default defineConfig({
   //   'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   outputPath: 'docs-dist',
   // // mode: 'site',
+  base: process.env.NODE_ENV === 'production' ? '/carefree-antd/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/carefree-antd/' : '/',
   proxy: {
     '/api': 'http://39.99.44.108:6666',
   },
