ea03e4f09224c1be8d47512354eedce363c789dd
SunLxy <1011771396@qq.com>
2025-04-13 10:42:03 +0800

    fix:优化配置

---
+++ packages/rslib.config/src/rslib.config.ts
@@ -0,0 +1,40 @@
+import { pluginReact } from '@rsbuild/plugin-react';
+import { defineConfig } from '@rslib/core';
+
+export default defineConfig({
+  source: {
+    entry: {
+      index: ['./src/**', '!src/**/*.md'],
+    },
+  },
+  lib: [
+    {
+      bundle: false,
+      dts: true,
+      format: 'esm',
+      output: {
+        filename: {
+          js: '[name].js',
+        },
+        distPath: {
+          root: './esm',
+        },
+      },
+    },
+    {
+      bundle: false,
+      dts: true,
+      format: 'cjs',
+      output: {
+        filename: {
+          js: '[name].js',
+        },
+        distPath: {
+          root: './lib',
+        },
+      },
+    },
+  ],
+
+  plugins: [pluginReact()],
+});
